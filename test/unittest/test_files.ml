54ffcff1e9ed9d1c5139c9308ff5ed83e67775f3
kelvinqian00 <kelvinqian2@gmail.com>
2020-08-20 15:12:38 -0400

    Add recusion natodefa tests and rename odefa tests

--- test/unittest/test_files.ml
+++ test/unittest/test_files.ml
@@ -1203,7 +1203,7 @@ let tests =
     (* @ make_tests_from_dir "test-sources/odefa-fails" *)
     (* @ make_tests_from_dir "test-sources/odefa-input" *)
     (* @ make_tests_from_dir "test-sources/odefa-stack" *)
-    (* @ make_tests_from_dir "test-sources/odefa-types" *)
+    @ make_tests_from_dir "test-sources/odefa-types"
     (* @ make_tests_from_dir "test-sources/natodefa-basic" *)
     @ make_tests_from_dir "test-sources/natodefa-types"
     (* @ make_tests_from_dir "test-sources/natodefa-input" *)
